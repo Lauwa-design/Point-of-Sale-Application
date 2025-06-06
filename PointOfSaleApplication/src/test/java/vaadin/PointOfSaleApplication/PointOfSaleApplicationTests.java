2577b9c1a9ece03ce74dad4ebc3c3ad69521155f
Lauwa-design <lauwaniki@gmail.com>
2025-06-06 10:36:20 +0300

    Link local project with remote repo

---
+++ PointOfSaleApplication/src/test/java/vaadin/PointOfSaleApplication/PointOfSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package vaadin.PointOfSaleApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
